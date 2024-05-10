a294a86db77f56ae5861ce01a944bc699f65dd97
MrMetalero <danymena7@gmail.com>
2024-05-10 14:01:19 +0200

    fakfafa

--- Triggers/triggers.sql
+++ Triggers/triggers.sql
@@ -5,13 +5,20 @@
 --como entrada el nombre de un pueblo y devuelva el número de institutos que hay en ese pueblo
 --SELECT contar_instituts('Ontinyent');
 
-CREATE FUNCTION contar_institutos() AS $contar_institutos$
+CREATE FUNCTION contar_institutos(nombre_pueblo character varying) 
+
+RETURNS integer AS
+$$                          --Este es el indicador del cuerpo de la función
+    DECLARE
+        numero_instituts integer; --Hay que poner el tipo al declarar la variable--
     BEGIN
-    SELECT COUNT(nombre_pueblo) FROM tabla_pueblos;
-
-
+        SELECT COUNT(nombre_pueblo) INTO numero_instituts
+        FROM instituts INNER JOIN poblacions ON instituts.cod_m = poblacions.cod_m
+        WHERE 
+        ;    
     END
-
+$$
+LANGUAGE plpgsql            --Este es el lenguage que utiliza la función.
 
 
 
